9b5a91a5a784560953b8b39e9fe41f91a9eca3c3
codingmax <codingmax@tv.local>
2024-03-24 22:33:58 +0900

    remove .visionos until github actions supports the swift-tools 5.9

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "KVObservable",
-    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v8), .tvOS(.v13), .macCatalyst(.v13), .visionOS(.v1)],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v8), .tvOS(.v13), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
